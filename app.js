2367ff7f3a116b7b117c23543155ecdf14e37bc1
unknown <contactsobhi@gmail.com>
2023-07-15 13:20:19 +0100

    level 4 security (hashing with bcrypt)

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 30;
 const app = express();
 
 app.use(express.static("public"));
@@ -31,31 +32,40 @@ const userSchema = new mongoose.Schema({
 const User = new mongoose.model("User",userSchema);
 
 app.post("/register",(req,res)=> {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+            
+        });
+        
+        newUser.save().then(() => {
+            console.log('User saved!');
+            res.render("secrets");
+            })
+    .catch(function (err) {
+            console.log(err);
+          });
     });
-    
-    newUser.save().then(() => {
-        console.log('User saved!');
-        res.render("secrets");
-        })
-.catch(function (err) {
-        console.log(err);
-      });
+
     
     });
     
     app.post("/login",(req,res)=> {
         const username = req.body.username;
-        const password = md5(req.body.password);
+        const password = req.body.password;
 
         User.findOne({email: username}).then(function (foundUser) {
             if(foundUser) {
-                if(foundUser.password === password) {
-                    res.render("\secrets");
-                    console.log(foundUser.password);
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result) {
+                        res.render("\secrets");
+                        console.log(foundUser.password);
+                        console.log(password);
+                    }
+                });
+   
+                
             }
              
             }).catch(function (err) {
